4dd4c394f47033e7356bfc54172d06b12dbd430a
nashht <nashht@nashhtdemacbook-pro.local>
2016-04-14 22:26:42 +0800

    界面

--- p2pChat/classes/Recent/RecentCell.m
+++ p2pChat/classes/Recent/RecentCell.m
@@ -44,6 +44,21 @@
     
 }
 
+#pragma mark - 绘制Cell分割线
+- (void)drawRect:(CGRect)rect {
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetFillColorWithColor(context, [UIColor clearColor].CGColor);
+    CGContextFillRect(context, rect);
+    
+    //上分割线，
+    //    CGContextSetStrokeColorWithColor(context, [UIColor colorWithRed:198/255.0 green:198/255.0 blue:198/255.0 alpha:1].CGColor);
+    //    CGContextStrokeRect(context, CGRectMake(0, 0, rect.size.width, 1));
+    
+    //下分割线
+    CGContextSetStrokeColorWithColor(context, [UIColor colorWithRed:198/255.0 green:198/255.0 blue:198/255.0 alpha:1].CGColor);
+    CGContextStrokeRect(context, CGRectMake(0, rect.size.height, rect.size.width, 1));
+}
 
 
 @end
