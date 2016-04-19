5518d7af58a14932fc37c690e50a86e008297292
nashht <nashht@nashhtdemacbook-pro.local>
2016-04-19 11:46:38 +0800

    创建群的界面。。选中。。

--- p2pChat/classes/FriendInfo/MeController.m
+++ p2pChat/classes/FriendInfo/MeController.m
@@ -34,12 +34,18 @@
     [[MyXMPP shareInstance] creatGroupName:@"113" withpassword:nil andsubject:nil];
     [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(invitenewfriends) name:MyXmppRoomDidConfigurationNotification object:nil];
     
+    
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     self.tabBarController.tabBar.hidden = NO;
 }
 
+//- (void)invitenewfriends{
+//   [[MyXMPP shareInstance] inviteFriends:@"cxh" withMessage:@"wewe"];
+//   [[MyXMPP shareInstance] inviteFriends:@"ht_test" withMessage:@"wewe"];
+//
+//}
 - (void)invitenewfriends{
    [[MyXMPP shareInstance] inviteFriends:@"cctest" withMessage:@"aaa"];
     
