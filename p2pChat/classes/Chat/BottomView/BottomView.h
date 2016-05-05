ff74bf48056f8c32ab65464c11637aab285f103d
今天天气不错 <584479645@qq.com>
2016-05-05 10:34:45 +0800

    - -

---
+++ p2pChat/classes/Chat/BottomView/BottomView.h
@@ -0,0 +1,28 @@
+//
+//  BottomView.h
+//  p2pChat
+//
+//  Created by xiaokun on 16/4/14.
+//  Copyright © 2016年 xiaokun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol BottomViewDelegate <NSObject>
+
+@required
+- (void)showMoreView;
+- (void)hideMoreView;
+- (void)showFaceView;
+@end
+
+@interface BottomView : UIView
+
+@property (copy, nonatomic) NSString *chatObjectString;
+@property (weak, nonatomic) id<BottomViewDelegate> delegate;
+@property (assign, nonatomic, getter=isP2PChat) BOOL p2pChat;
+
+- (void)resignTextfield;
+- (void)inputFaceView:(NSString *)faceName;
+
+@end
