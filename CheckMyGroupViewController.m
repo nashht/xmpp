755bf64ad57f096a1998e3be6200a60d7239c791
今天天气不错 <584479645@qq.com>
2016-06-10 21:55:58 +0800

    all

--- CheckMyGroupViewController.m
+++ CheckMyGroupViewController.m
@@ -7,17 +7,28 @@
 //
 #define ID  @"MyGroupCell"
 #import "CheckMyGroupViewController.h"
+#import "MyXMPP+Group.h"
+#import "ChatViewController.h"
+
 
 @interface CheckMyGroupViewController()<UITableViewDelegate,UITableViewDataSource>
-
+@property (nonatomic, strong) NSArray *members;
 @end
 
 @implementation CheckMyGroupViewController
 
-- (void)viewDidLoad{
-    self.view.backgroundColor = [UIColor grayColor];
 
+- (void)viewDidLoad{
+    [super viewDidLoad];
+    
+    self.navigationItem.title = @"我加入的群";
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:ID];
+    
+    [[MyXMPP shareInstance]fetchMyRoomsWithCompletion:^(NSArray *members) {
+        _members = members;
+        [self.tableView reloadData];
+    }];
+    
 }
 
 
@@ -28,7 +39,7 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 3;
+    return _members.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
@@ -37,10 +48,26 @@
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:ID];
     }else{
         cell = [[UITableViewCell alloc] init];
-        cell.backgroundColor = [UIColor redColor];
     }
+    cell.textLabel.text = _members[indexPath.row];
     
     return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    NSString *name = _members[indexPath.row];
+    //    NSArray *options = @[name, @0];
+    
+    //    ChatViewController *chatVc = [[ChatViewController alloc] init];
+    //    [self.navigationController showViewController:chatVc sender:options];
+    
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    ChatViewController *viewController = [storyboard instantiateViewControllerWithIdentifier:@"chat"];
+    viewController.chatObjectString = name;
+    viewController.p2pChat = NO;
+    [self.navigationController pushViewController:viewController animated:YES];
+//    [self.navigationController showViewController:viewController sender:nil];
 }
 
 @end
