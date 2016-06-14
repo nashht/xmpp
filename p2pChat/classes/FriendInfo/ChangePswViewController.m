8cc98c3b7aa676712373dfb5484ce7e1e12bf208
admin <admin@admindemacbook-pro.local>
2016-06-14 21:59:53 +0800

    nil

--- p2pChat/classes/FriendInfo/ChangePswViewController.m
+++ p2pChat/classes/FriendInfo/ChangePswViewController.m
@@ -20,19 +20,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    MyXMPP *xmpp = [MyXMPP shareInstance];
-    NSString *name = [[NSUserDefaults standardUserDefaults]valueForKey:@"name"];
-    if (([_newpsw.text length]!=0)&&([_newpswagain.text length]!=0)) {
-        BOOL result = [_newpsw.text isEqualToString: _newpswagain.text];
-        
-        if (result&&([name isEqualToString:xmpp.myjid.user])) {
-            [[NSUserDefaults standardUserDefaults]setValue:_newpsw forKey:@"password"];
-        }
-        
-    }
+    self.navigationItem.title = @"密码设置";
+//    self.navigationController.navigationBar.tintColor = []
+    
+    
+    
+    
+
     
 }
 
+- (IBAction)changePsw:(id)sender {
+    
+    MyXMPP *xmpp = [MyXMPP shareInstance];
+    NSString *name = [[NSUserDefaults standardUserDefaults]valueForKey:@"name"];
+        if (([_newpsw.text length]!=0)&&([_newpswagain.text length]!=0)) {
+            BOOL result = [_newpsw.text isEqualToString: _newpswagain.text];
+    
+            if (result&&([name isEqualToString:xmpp.myjid.user])) {
+                [[NSUserDefaults standardUserDefaults]setValue:_newpsw forKey:@"password"];
+            }
+            
+        }
+    
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
